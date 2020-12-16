a86c0dc73cd93a0f10102431b359770936b7b5e4
liujun <lj2010vip@163.com>
2020-12-16 22:45:02 +0800

    eureka快速移除失效服务

---
+++ orjrs-techstack-server/techstack-security/src/main/java/com/orjrs/techstack/security/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.orjrs.techstack.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    protected void configure(HttpSecurity http) throws Exception {
+        // 关闭csrf
+        http.csrf().disable();
+        // 支持httpBasic
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+}
