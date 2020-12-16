63b318080d82043235f2873749bec5768121938e
liujun <lj2010vip@163.com>
2020-12-16 21:31:11 +0800

    生产者：登陆

---
+++ orjrs-techstack-server/techstack-login/src/main/java/com/orjrs/techstack/login/LoginApplication.java
@@ -0,0 +1,16 @@
+package com.orjrs.techstack.login;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+//@EnableEurekaClient
+@EnableDiscoveryClient
+public class LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginApplication.class, args);
+	}
+
+}
