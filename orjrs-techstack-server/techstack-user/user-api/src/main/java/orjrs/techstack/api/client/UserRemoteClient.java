2975aa3e2fdb4a17e1e597bab550af82d6960f88
liujun <lj2010vip@163.com>
2020-12-28 21:24:54 +0800

    feign 配置

--- orjrs-techstack-server/techstack-user/user-api/src/main/java/orjrs/techstack/api/client/UserRemoteClient.java
+++ orjrs-techstack-server/techstack-user/user-api/src/main/java/orjrs/techstack/api/client/UserRemoteClient.java
@@ -3,7 +3,7 @@ package orjrs.techstack.api.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient("user")
+@FeignClient("user-provider")
 public interface UserRemoteClient {
 
     @GetMapping("/user/name")
